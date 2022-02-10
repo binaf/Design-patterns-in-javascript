93bd0903e2947c47b10548634a42639757cac8bf
binaf <ousmanehaidara26@gmail.com>
2022-02-10 19:55:56 +0100

    closure step 01

--- index.js
+++ index.js
@@ -11,6 +11,54 @@ let instance01 = {};
 let instance02 = Object.create(Object.prototype);
 let instance03 = new Object();
 
-console.log('instance01', instance01);
-console.log('instance02', instance02);
-console.log('instance03', instance03);
+// console.log('instance01', instance01);
+// console.log('instance02', instance02);
+// console.log('instance03', instance03);
+
+// 2 Module Pattern
+
+//  +++++++++++++++++ closure
+
+// We use a immediately invoked function to create
+// a private variable counter
+var counterIncrementer = (() => {
+  let counter = 0;
+  return function () {
+    return ++counter;
+  };
+})(); // these () in the end make this a immediately invoked function
+
+// // prints: 1
+// console.log(counterIncrementer());
+// // prints: 2
+// console.log(counterIncrementer());
+// // prints: 3
+// console.log(counterIncrementer());
+
+// Using a closure we will expose an object
+// as part of a public API that manages its
+// private parts
+let fruitsCollection = (() => {
+  // private
+  let objects = [];
+
+  //public
+  return {
+    addObject: (object) => {
+      objects.push(object);
+    },
+    removeObject: (object) => {
+      let index = objects.indexOf(object);
+      if (index >= 0) {
+        objects.splice(index);
+      }
+    },
+    getObjects: () => JSON.parse(JSON.stringify(objects)),
+  };
+})();
+
+fruitsCollection.addObject('apple');
+fruitsCollection.addObject('mange');
+fruitsCollection.addObject('pain');
+fruitsCollection.addObject('lait');
+console.log(fruitsCollection.getObjects());
